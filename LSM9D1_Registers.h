1cb7bdd154ddaecbbabb82dfc4aea86dc3bd3cfd
Jan Swoboda <99872369+Fquarl@users.noreply.github.com>
2023-08-18 14:41:14 +0200

    Update LSM9D1_Registers.h

--- LSM9D1_Registers.h
+++ LSM9D1_Registers.h
@@ -54,32 +54,32 @@
 #define INT_GEN_DUR_G       0x37
 
 // Magnet Registers
-#define OFFSET_X_REG_L_M	0x05
-#define OFFSET_X_REG_H_M	0x06
-#define OFFSET_Y_REG_L_M	0x07
-#define OFFSET_Y_REG_H_M	0x08
-#define OFFSET_Z_REG_L_M	0x09
-#define OFFSET_Z_REG_H_M	0x0A
-#define WHO_AM_I_M			0x0F
-#define CTRL_REG1_M			0x20
-#define CTRL_REG2_M			0x21
-#define CTRL_REG3_M			0x22
-#define CTRL_REG4_M			0x23
-#define CTRL_REG5_M			0x24
-#define STATUS_REG_M		0x27
-#define OUT_X_L_M			0x28
-#define OUT_X_H_M			0x29
-#define OUT_Y_L_M			0x2A
-#define OUT_Y_H_M			0x2B
-#define OUT_Z_L_M			0x2C
-#define OUT_Z_H_M			0x2D
-#define INT_CFG_M			0x30
-#define INT_SRC_M			0x31
-#define INT_THS_L_M			0x32
-#define INT_THS_H_M			0x33
+#define OFFSET_X_REG_L_M	  0x05
+#define OFFSET_X_REG_H_M	  0x06
+#define OFFSET_Y_REG_L_M	  0x07
+#define OFFSET_Y_REG_H_M	  0x08
+#define OFFSET_Z_REG_L_M	  0x09
+#define OFFSET_Z_REG_H_M	  0x0A
+#define WHO_AM_I_M			    0x0F
+#define CTRL_REG1_M			    0x20
+#define CTRL_REG2_M			    0x21
+#define CTRL_REG3_M			    0x22
+#define CTRL_REG4_M			    0x23
+#define CTRL_REG5_M			    0x24
+#define STATUS_REG_M		    0x27
+#define OUT_X_L_M			      0x28
+#define OUT_X_H_M			      0x29
+#define OUT_Y_L_M			      0x2A
+#define OUT_Y_H_M			      0x2B
+#define OUT_Z_L_M			      0x2C
+#define OUT_Z_H_M			      0x2D
+#define INT_CFG_M			      0x30
+#define INT_SRC_M			      0x31
+#define INT_THS_L_M			    0x32
+#define INT_THS_H_M			    0x33
 
 // Responses
-#define WHO_AM_I_AG_RSP		0x68
-#define WHO_AM_I_M_RSP		0x3D
+#define WHO_AM_I_AG_RSP		  0x68
+#define WHO_AM_I_M_RSP		  0x3D
 
 #endif
