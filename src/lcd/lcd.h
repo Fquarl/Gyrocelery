8e5c0ff5f2be4e8149782dd04a901f6d72645af1
Luca Hesselbrock <luca.hesselbrock@web.de>
2023-10-16 14:44:48 +0200

    Added file 'lcd.h' for support of given LCD-display.

---
+++ src/lcd/lcd.h
@@ -0,0 +1,7 @@
+/*!
+*   \file       lcd.h
+*   \date       16.10.2023
+*   \author     Luca Hesselbrock
+*   \copyright  Copyright (C) Gyrocelery 2023
+*   \brief      Library for support of the given LCD-display
+*/
