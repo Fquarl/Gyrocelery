f39d46b66038348d6519609a9c59330c0bbcb433
Lucas <lucas.niemeyer@zf.com>
2023-12-13 13:51:42 +0100

    Driver moved

--- lib/i2c_driver/i2c_driver.h
+++ lib/i2c_driver/i2c_driver.h
@@ -6,13 +6,24 @@
 
 #include <Arduino.h>
 
+void i2c_init();
+
 void i2c_startCondition();
 void i2c_repeatedStartCondition();
-void i2c_sendAddressWrite(const int address);
-void i2c_sendAddressRead(const int address);
 void i2c_stopCondition();
-void i2c_sendData(int data);
+
+int i2c_sendAddressWrite(const int address);
+int i2c_sendAddressRead(const int address);
+
+int i2c_sendData(int data);
+
 void i2c_writeByte(int _byte);
-int i2c_readByte();
+int i2c_readByte(bool continueTransfer = false);
+
+int i2c_writeWord(int address, int peripheralRegister, int data);
+int i2c_readWord(int address, int peripheralRegister, char* data);
+
+int i2c_readDWord(int address, int peripheralRegister, int* data);
+int i2c_readNDwords(int address, int startRegister, int* dataArray, int len);
 
 #endif
