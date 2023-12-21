b3d0bd85529a56320b6ad665fd482d3544894c0b
Hessi02 <131236333+Hessi02@users.noreply.github.com>
2023-12-21 14:15:23 +0100

    Changed formatting for pointer alignment.

--- lib/Vector_header_only/vector_heap.hpp
+++ lib/Vector_header_only/vector_heap.hpp
@@ -19,11 +19,11 @@ namespace VECTOR_HEAP
      *  \brief      This group contains basic settings for the ATmega32's memory
      */
 
-    /**
-     *  \var        totalLenghtOfSRAM
-     *  \brief      The constant specifies the total count of bytes in the SRAM
-     *  \value      0x800
-     */
+     /**
+      *  \var        totalLenghtOfSRAM
+      *  \brief      The constant specifies the total count of bytes in the SRAM
+      *  \value      0x800
+      */
     constexpr uin16_t totalLenghtOfSRAM = 0x800;
 
     /**
@@ -33,7 +33,7 @@ namespace VECTOR_HEAP
      *  \ingroup    HeapSettings
      *  \value      The default value is set to 64 byte.
      */
-    constexpr size_t &mallocMargin = __malloc_margin;
+    constexpr size_t& mallocMargin = __malloc_margin;
     mallocMargin = 64;
 
     /**
@@ -41,24 +41,22 @@ namespace VECTOR_HEAP
      *  \brief      The constant specifies the start of the heap
      *  \ingroup    HeapSettings
      */
-    constexpr char *&mallocHeapStart = __malloc_heap_start;
+    constexpr char*& mallocHeapStart = __malloc_heap_start;
 
     /**
      *  \var        mallocHeapEnd
      *  \brief      The constant specifies the end of the heap
      *  \ingroup    HeapSettings
      */
-    constexpr char *&mallocHeapEnd = __malloc_heap_end;
+    constexpr char*& mallocHeapEnd = __malloc_heap_end;
 
     /**
      *  \fn         new
      *  \brief      The function creates a new instance on the heap
      *  \param[in]  size passes the size of the new instance in bytes
-     *  \return     A pointer to the created instance on the heap.
-     *  \return     nullptr if the memory allocation fails!
      *  \note       The function might return nullptr to avoid heap and stack colliding!
      */
-    void *operator new(size_t size)
+    void* operator new(size_t size)
     {
         return malloc(size);
     }
@@ -68,7 +66,7 @@ namespace VECTOR_HEAP
      *  \brief      The function deletes a existing instance from the heap
      *  \param[in]  prt passes the pointer to the instance to delete from the heap
      */
-    void operator delete(void *ptr)
+    void operator delete(void* ptr)
     {
         free(ptr);
     }
