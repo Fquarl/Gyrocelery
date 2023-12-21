195015101fdce7296adbb0b5cf3cb4195741c73c
Hessi02 <131236333+Hessi02@users.noreply.github.com>
2023-12-21 11:55:28 +0100

    Safety features for the heap allocation.
    The added code provides functions with the purpose to avoid a collision between Stack and Heap. Therefore a minimal margin is set which only can be crossed by stack expansion. Also the maximum heap size is set to 10% of the ATmega32's provided SRAM.

--- lib/Vector_header_only/vector_heap.hpp
+++ lib/Vector_header_only/vector_heap.hpp
@@ -15,6 +15,36 @@
 namespace VECTOR_HEAP
 {
     /**
+     *  \defgroup   HeapSettings
+     *  \brief      This group contains basic settings for the ATmega32's memory
+     */
+
+    /**
+     *  \var        totalLenghtOfSRAM
+     *  \brief      The constant specifies the total count of bytes in the SRAM
+     *  \value      0x800
+     */
+    constexpr uin16_t totalLenghtOfSRAM = 0x800;
+
+    /**
+     *  \var        __malloc_margin
+     *  \brief      The constant specifies the minimum margin between stack and heap
+     *  \note       The margin can be undercut if the stack grows after heap expansion!
+     *  \ingroup    HeapSettings
+     *  \value      The default value is set to 64 byte.
+     */
+    __malloc_margin = 64;
+
+    /**
+     *  \var        __malloc_heap_end
+     *  \brief      The constant specifies the end of the heap
+     *  \note       Due to stack-heap-collision, only 0x80 bytes can be allocated!
+     *  \ingroup    HeapSettings
+     *  \value      The heap length is set to 10% of the total length of SRAM
+     */
+    __malloc_heap_end = __malloc_heap_start + totalLenghtOfSRAM / 10;
+
+    /**
      *  \fn         new
      *  \brief      The function creates a new instance on the heap
      *  \param[in]  size passes the size of the new instance in bytes
