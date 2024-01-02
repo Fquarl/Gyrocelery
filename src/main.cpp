7d9a9a3f13b40905eddfa3c33563399f7610232d
OliTat <olivertatura@mailbox.org>
2024-01-02 13:52:10 +0100

    Changed Makefile for c/cpp
    Makefile now correctly handles c AND cpp sources (hopefully)

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <avr/io.h>			// Einbinden von Einstellungen/Definitionen/usw. für den Mikrocontroller
-#define F_CPU 8000000UL			// Frequenz des Microcontrollers fuer delay.h festlegen
-#include <util/delay.h>			// Einbinden der _delay_ms()-Funktion um Wartezeiten zu erzeugen
-#include "../lib/i2c_driver/i2c_driver.h"
-
-int main (void)				// Hauptprogramm, hier startet der Mikrocontroller
-{
-	i2c_pfusch();					// Wartezeit von 500ms
-}
