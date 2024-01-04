99d05910b48a1e31d5078d42f278507fcf121db5
Hessi02 <131236333+Hessi02@users.noreply.github.com>
2024-01-04 09:23:12 +0100

    Added useful operators

--- lib/Vector_header_only/vector.hpp
+++ lib/Vector_header_only/vector.hpp
@@ -15,6 +15,27 @@
 template <typename T>
 class Vector : public VectorContainer<T>
 {
+    /**
+     *  \fn         []
+     *  \brief      The operator returns the value at the passed index
+     *  \param[in]  index passes the index of the target value
+     *  \return     The value at the given index
+     *  \see        at
+     */
+    T& operator [](const uint8_t &index)
+    {
+        return at(index);
+    }
+
+    /**
+     *  \fn         <<
+     *  \brief      The operator adds a new element to the top of the linked list
+     *  \param[in]  value passes the instance of template type to store
+     */
+    void operator <<(const T value)
+    {
+        append(value);
+    }
 };
 
 #endif //__VECTOR_HPP__
